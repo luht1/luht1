fe9e03e1e262b023b6b63d14da36a7aabd11a9af
luhantai <19802021149@139.com>
2019-04-12 14:33:54 +0800

    1

---
+++ src/main/java/com/luht/gitshare/dao/testDao.java
@@ -0,0 +1,13 @@
+package com.luht.gitshare.dao;
+
+/**
+ * @author luht
+ * @date 2019/4/12 14:33
+ * @mail lu_hantai@139.com
+ */
+public class testDao {
+
+    public String get(){
+        return null;
+    }
+}
