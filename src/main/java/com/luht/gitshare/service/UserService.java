c1062fe3b0d4717458747064f2432a85d3d856e2
luhantai <19802021149@139.com>
2019-04-12 14:43:17 +0800

    Create UserService.java
    1

---
+++ src/main/java/com/luht/gitshare/service/UserService.java
@@ -0,0 +1,12 @@
+package com.luht.gitshare.service;
+
+/**
+ * @author luht
+ * @date 2019/4/12 14:34
+ * @mail lu_hantai@139.com
+ */
+public class UserService {
+    public String getUserInfo(String id){
+        return null;
+    }
+}
